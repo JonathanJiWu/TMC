b3d8320b9d0181f4642d20433075193bf98136d3
JJW <jonathanwubrave@gmail.com>
2021-10-15 23:14:15 -0400

    Update Orwell.java

--- src/TMC/Orwell.java
+++ src/TMC/Orwell.java
@@ -1,31 +1,20 @@
 
 import java.util.Scanner;
 
-public class OddOrEven {
+public class Password {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Give a number:");
-        int input = scan.nextInt();
-        if (input%2==0){
-            System.out.println("Number "+input+" is even.");
-        }else {
-            System.out.println(("Number "+input+" is odd."));
-        }
 
         // Write your program here
-        // HINT:
-        // You can find out if a number is even or odd easily using the modulo operator %
-        // Try the following commands to see what they print
-        // System.out.println( 1%2 );
-        // System.out.println( 2%2 );
-        // System.out.println( 3%2 );
-        // System.out.println( 4%2 );
-        // System.out.println( 5%2 );
-        // System.out.println( 6%2 );
-        // System.out.println( 7%2 );
-        // int luku = 8
-        // System.out.println( luku%2 );
-        // So, by taking the modulo of a number and two you can find out if it is even or odd !
+        System.out.println("Enter the first string:");
+        String input = scan.nextLine();
+        System.out.println("Enter the second string:");
+        String input2 = scan.nextLine();
+        if (input.equals(input2)) {
+            System.out.println("Same!");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
